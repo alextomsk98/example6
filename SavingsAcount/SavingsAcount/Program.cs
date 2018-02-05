dd4054d07ab137d9be9d33a9b0b5eca66488a693
alextomsk98 <shavlo.2016@mail.ru>
2018-02-05 18:27:18 +0700

    laba1(5)

---
+++ SavingsAcount/SavingsAcount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingsAcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Data*****\n");
+            SavingsAcount s1 = new SavingsAcount(50);
+            Console.WriteLine("Interest Rate is:{0}", SavingsAcount.GetInterestRate());
+            SavingsAcount.SetInterestRate(0.08);
+            SavingsAcount s2 = new SavingsAcount(100);
+            Console.WriteLine("Interest Rate is:{0}", SavingsAcount.GetInterestRate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SavingsAcount/SavingsAcount/SavingsAcount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingsAcount
+{
+    class SavingsAcount
+    {
+        public double currBalance;
+        public static double currInterestRate;
+        public SavingsAcount(double balance)
+        {
+            currInterestRate = 0.04;
+            currBalance = balance;
+        }
+        static SavingsAcount()
+        {
+            Console.WriteLine("in static ctor!");
+            currInterestRate = 0.04;
+        }
+        public static void SetInterestRate(double newRate)
+        {
+            currInterestRate = newRate;
+        }
+        public static double GetInterestRate()
+        {
+            return currInterestRate;
+        }
+    }
+}
